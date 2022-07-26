0b65c65542eb827c221a73c002abcf0b6a4ceabf
Amar <amar1998fzd@gmail.com>
2022-07-27 01:38:04 +0530

    Creating React App

---
+++ first-project/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Box from './components/Box';
+
+function App() {
+  return (
+    <div className="App">
+      <Box/>
+      <Box/>
+      <Box/>
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ first-project/src/components/Box.js
@@ -0,0 +1,10 @@
+function Box() {
+    return (
+        <div className="main-div">
+        <h1>All Questions</h1>
+        <p>This box will tell the number of questions in today's assignment</p>
+        <button id="join-btn">Join the meeting</button>
+        </div> 
+    );
+  }
+  export default Box;  
